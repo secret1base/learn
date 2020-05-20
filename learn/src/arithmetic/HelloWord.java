9f5689db0995e991706c747ed0ed51bb18943ee9
wuyunjia <18951603271@189.cn>
2020-05-20 11:29:45 +0800

    test

---
+++ learn/src/arithmetic/HelloWord.java
@@ -0,0 +1,11 @@
+package arithmetic;
+
+/**
+ * @author: wyj
+ * @create: 2020-05-20 11:14
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
